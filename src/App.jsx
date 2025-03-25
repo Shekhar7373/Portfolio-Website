878dc18b9051fc690909b05a6735ebeb99f39115
Shekhar Doddamani <129622008+Shekhar7373@users.noreply.github.com>
2025-03-25 17:28:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import styles from "./App.module.css";
 import { About } from "./components/About/About";
-import { Contact } from "./react-portfolio-main/src/components/Contact/Contact";
-import { Experience } from "./react-portfolio-main/src/components/Experience/Experience";
-import { Hero } from "./react-portfolio-main/src/components/Hero/Hero";
-import { Navbar } from "./react-portfolio-main/src/components/Navbar/Navbar";
-import { Projects } from "./react-portfolio-main/src/components/Projects/Projects";
+import { Contact } from "./components/Contact/Contact";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Projects } from "./components/Projects/Projects";
 
 function App() {
   return (
